5338af3c532dcd2170b8ca7200b3c969eaad1016
Naveed Hassan <120362779+NaveedBaloch12@users.noreply.github.com>
2023-04-08 21:15:49 +0500

    Add files via upload

---
+++ 3..Product of two numbers.cpp
@@ -0,0 +1,16 @@
+//Multiplication of two numbers
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"This program will multiply two numbers"<<endl;
+	int a,b,c;
+	cout<<"\aInput First Number= ";
+	cin>>a;
+	cout<<"Input Second Number= ";
+	cin>>b;
+	c=a*b;
+	cout<<"Product of two numbers is= "<<c;
+}
---
+++ 4..Square of a number.cpp
@@ -0,0 +1,14 @@
+//Square or a number
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout <<"This Program will Take square of a number.\n";
+	cout <<"Type Number= ";
+	cin>> a;
+	b=a*a;
+	cout<<"Square of "<< a << " is= "<<b;
+}
